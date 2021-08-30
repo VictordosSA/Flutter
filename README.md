4e5d601b243b78c2a249688b52ac9cb62954ed4c
Victor Amaral <victorsantosamaral2014@gmail.com>
2021-08-29 20:58:35 -0400

    Commit Full do Projeto do Flutter_CRUD

---
+++ README.md
@@ -0,0 +1,20 @@
+# flutter_crud
+
+A new Flutter project.
+
+## Getting Started
+
+É um projeto de Flutter CRUD -> Aplicação de Cdastro Usuário, para esse projeto temos a possibilidade de Cria, Editar e Excluir usuário via Formulários.
+
+By: Victor Amaral for COD3R
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email:'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331256_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email:'rafael@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png',
+  ),
+   '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email:'fernanda@zymail.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2017/07/18/23/23/user-2517433_960_720.png',
+  ),
+   '4': const User(
+    id: '4',
+    name: 'Guilherme',
+    email:'guigui@abcmail.com.br',
+    avatarUrl: '',
+  )
+
+};
